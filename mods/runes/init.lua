61f99b0b55e063fe78c96cd2650946a77b9f0b58
LeMagnesium <mg.minetest@gmail.com>
2014-12-13 01:01:10 +0100

    Runes: Filtres de couleurs pour les tables - Modifications du haut de la table active - Ajout des fonctionnalites de filtres colores pour les tables suivant la   rune utilisee. (necessite un client >0.4.10)

--- mods/runes/init.lua
+++ mods/runes/init.lua
@@ -18,12 +18,12 @@
 local runes = {}
 
 runes.runes = {
-	{"smith","Rune du Forgeron"},
-	{"thief","Rune du Voleur"},
-	{"assassin","Rune de l'Assassin"},
-	{"pyromaniac","Rune du Pyromane"},
-	{"necromancer","Rune du Nécromancien"},
-	{"mortis_angelum","Rune de l'Ange de la Mort"},
+	{"smith","Rune du Forgeron","#00FF00"},
+	{"thief","Rune du Voleur","#667722"},
+	{"assassin","Rune de l'Assassin","#888888"},
+	{"pyromaniac","Rune du Pyromane","#FFBB11"},
+	{"necromancer","Rune du Nécromancien","#660000"},
+	{"mortisangelum","Rune de l'Ange de la Mort","#222222"},
 }
 
 -- Enregistrement de la table vide.
@@ -42,7 +42,7 @@ minetest.register_node ("runes:table_empty", {
 		local meta = minetest.get_meta(pos)
 		local inv  = meta:get_inventory()
 		
-		meta:set_string("infotext","Viens a moi apprenti magicien...")
+		meta:set_string("infotext","Viens a moi apprenti(e) magicien(ne)...")
 		meta:set_string("formspec",
 			"size[10,7]"..
 			"list[context;ginput;4,1;1,1;]"..
@@ -71,6 +71,7 @@ minetest.register_node ("runes:table_empty", {
 for _, row in ipairs(runes.runes) do
 	local code = row[1] -- Code d'identification de la rune
 	local desc = row[2] -- Description de la rune
+	local colo = row[3] -- Couleur associée
 	-- COMING SOON --
 	
 	-- Registering item : rune
@@ -82,8 +83,8 @@ for _, row in ipairs(runes.runes) do
 	
 	-- Registering node : table d'enchantement
 	minetest.register_node("runes:table_"..code, {
-		description = "Table d'enchantement active!",
-		tiles = {"table_active_top.png","table_active_bottom.png","table_active_side.png"},
+		description = "Table d'enchantement active",
+		tiles = {"(table_active_top.png^[colorize:"..colo..")^table_active_top.png","table_active_bottom.png","table_active_side.png"},
 		groups = {oddly_breakable_by_hand = 2},
 		drawtype = "nodebox",
 		paramtype = "light",
